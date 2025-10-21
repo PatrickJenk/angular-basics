228f27f59449514e6612004efd04c041f041046b
Patrick Jenk <patrick@clients.ch>
2025-10-21 19:01:30 +0200

    change

--- portfolio/src/app/services/cv-service.ts
+++ portfolio/src/app/services/cv-service.ts
@@ -8,10 +8,10 @@ export class CvService {
   
   get_cvs(): Cv[] {
     return [
-      new Cv('Seiffenkisten Bauer', 2007, 20012),
+      new Cv('Seifenkisten Bauer', 2007, 2012),
       new Cv('Pausen-Clown', 2013, 2018),
-      new Cv('Profi Handballer', 2018, 2023),
-      new Cv('Chrüppu EFZ', 2023),
+      new Cv('Profi Handballer', 2019, 2023),
+      new Cv('Chrüppu EFZ', 2024),
     ]
   }  
 }
