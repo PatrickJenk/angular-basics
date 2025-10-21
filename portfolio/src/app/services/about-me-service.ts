3185201235e77801fd2bd38c11bdf0ead5e77def
Patrick Jenk <patrick@clients.ch>
2025-10-21 18:51:18 +0200

    Change Picture

--- portfolio/src/app/services/about-me-service.ts
+++ portfolio/src/app/services/about-me-service.ts
@@ -8,7 +8,7 @@ export class AboutMeService {
  
   getAboutMe(): AboutMe {
     return new AboutMe(
-      'Patrick Jenk', 'https://static.wikitide.net/loathsomecharacterswiki/thumb/f/f6/C--Users-cleme-Downloads-527-5274029_patrick-star-clipart-patrick-star-drooling-png-download.png/300px-C--Users-cleme-Downloads-527-5274029_patrick-star-clipart-patrick-star-drooling-png-download.png'
+      'Patrick Jenk', 'https://lh3.googleusercontent.com/proxy/NlxjO6z4xOja8k0Upzy4T1oDjwtXT2eO8MtVwYnSpJwbtKYcuZvggFKqxkgqx9n7-N5FwO17Qm7EBCIaJRWHfZknFqYn'
     )
   }
 }
