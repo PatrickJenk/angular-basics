a8d51f92e5fe625187c774365a115bbf45b2938f
Patrick Jenk <patrick@clients.ch>
2025-11-11 16:02:53 +0100

    v11.

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AboutMeComponent } from './components/about-me-component/about-me-component';
 import { CvComponent } from './components/cv-component/cv-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutMeComponent, CvComponent],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AboutMeComponent, CvComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // <-- plural + Array
 })
 export class App {
   protected readonly title = signal('portfolio');
